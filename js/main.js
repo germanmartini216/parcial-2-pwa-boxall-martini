badbed621e98bb02a9a82a7079107a1c63321474
LucioBxll <lucio.boxall15@gmail.com>
2024-06-16 13:17:42 -0300

    Favoritos y boton de favoritos

--- js/main.js
+++ js/main.js
@@ -1,76 +1,76 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/js/service-worker.js').then(registration => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }).catch(error => {
-          console.error('ServiceWorker registration failed:', error);
-      });
-  });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/js/service-worker.js').then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(error => {
+            console.error('ServiceWorker registration failed:', error);
+        });
+    });
 }
 
-const apiKey = '50a718ad'; 
+const apiKey = '50a718ad';
 const movieTitles = [
-  'Inception',
-  'The Matrix',
-  'Interstellar',
-  'The Dark Knight',
-  'Pulp Fiction',
-  'Fight Club',
-  'Forrest Gump',
-  'The Shawshank Redemption',
-  'The Godfather',
-  'The Lord of the Rings: The Fellowship of the Ring',
-  'Star Wars: Episode IV - A New Hope',
-  'The Empire Strikes Back',
-  'The Silence of the Lambs',
-  'Schindler\'s List',
-  'Goodfellas'
+    'Inception',
+    'The Matrix',
+    'Interstellar',
+    'The Dark Knight',
+    'Pulp Fiction',
+    'Fight Club',
+    'Forrest Gump',
+    'The Shawshank Redemption',
+    'The Godfather',
+    'The Lord of the Rings: The Fellowship of the Ring',
+    'Star Wars: Episode IV - A New Hope',
+    'The Empire Strikes Back',
+    'The Silence of the Lambs',
+    'Schindler\'s List',
+    'Goodfellas'
 ];
 
 const url = 'http://www.omdbapi.com/';
 const CACHE_NAME = 'movies-cache-v1';
 
 async function fetchMovieDetails(title) {
-  const params = new URLSearchParams({
-      apikey: apiKey,
-      t: title,
-      plot: 'short'
-  });
+    const params = new URLSearchParams({
+        apikey: apiKey,
+        t: title,
+        plot: 'short'
+    });
 
-  const requestUrl = `${url}?${params}`;
-  try {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(requestUrl);
+    const requestUrl = `${url}?${params}`;
+    try {
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(requestUrl);
 
-      if (cachedResponse) {
-          console.log(`Cache hit for: ${title}`);
-          return await cachedResponse.json();
-      } else {
-          console.log(`Fetching from network: ${title}`);
-          const networkResponse = await fetch(requestUrl);
-          if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
-              cache.put(requestUrl, networkResponse.clone());
-          }
-          return await networkResponse.json();
-      }
-  } catch (error) {
-      console.error('Error fetching data:', error);
-      return null;
-  }
+        if (cachedResponse) {
+            console.log(`Cache hit for: ${title}`);
+            return await cachedResponse.json();
+        } else {
+            console.log(`Fetching from network: ${title}`);
+            const networkResponse = await fetch(requestUrl);
+            if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+                cache.put(requestUrl, networkResponse.clone());
+            }
+            return await networkResponse.json();
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
 }
 
 function displayMovies(movies) {
-  const moviesContainer = document.getElementById('movies-container');
-  if (!moviesContainer) {
-      console.error('movies-container not found');
-      return;
-  }
-  moviesContainer.innerHTML = '';
+    const moviesContainer = document.getElementById('movies-container');
+    if (!moviesContainer) {
+        console.error('movies-container not found');
+        return;
+    }
+    moviesContainer.innerHTML = '';
 
-  movies.forEach(movie => {
-      if (movie && movie.Response === "True") {
-        const isFavorite = favoriteMovies.includes(movie.imdbID);
-          const movieCard = `
+    movies.forEach(movie => {
+        if (movie && movie.Response === "True") {
+            const isFavorite = favoriteMovies.includes(movie.imdbID);
+            const movieCard = `
               <div class="col s12 m6 l3">
                   <div class="card movie-card hoverable">
                       <div class="card-image">
@@ -83,18 +83,19 @@ function displayMovies(movies) {
                           <p><strong>IMDb Rating:</strong> ${movie.imdbRating}</p>
                       </div>
                       <div class="card-action">
-                            <a class="waves-effect waves-light btn ${isFavorite ? 'red' : ''}" onclick="toggleFavorite('${movie.imdbID}')">
-                                ${isFavorite ? 'Quitar' : 'Favoritos'}
+                            <a class="waves-effect waves-light btn-small ${isFavorite ? 'red' : 'amber'}" onclick="toggleFavorite('${movie.imdbID}')">
+                                ${isFavorite ? '<i class="material-icons">favorite</i>' : '<i class="material-icons">favorite_border</i>'}
                             </a>
+                            <a class="waves-effect waves-light btn-small amber">Detalles</a>
                         </div>
                   </div>
               </div>
           `;
-          moviesContainer.innerHTML += movieCard;
-      } else {
-          console.warn(`pelicula no encontrada: ${JSON.stringify(movie)}`);
-      }
-  });
+            moviesContainer.innerHTML += movieCard;
+        } else {
+            console.warn(`pelicula no encontrada: ${JSON.stringify(movie)}`);
+        }
+    });
 }
 
 function toggleFavorite(movieId) {
@@ -105,25 +106,25 @@ function toggleFavorite(movieId) {
         favoriteMovies.splice(index, 1);
     }
     saveFavoriteMovies(favoriteMovies);
-    fetchAndDisplayMovies(); 
+    fetchAndDisplayMovies();
 }
 
 let favoriteMovies = loadFavoriteMovies();
-fetchAndDisplayMovies(); 
+fetchAndDisplayMovies();
 function fetchAndDisplayMovies() {
-  const moviePromises = movieTitles.map(title => fetchMovieDetails(title));
-  Promise.all(moviePromises).then(movies => {
-      console.log('All movies fetched:', movies);
-      displayMovies(movies);
-  });
+    const moviePromises = movieTitles.map(title => fetchMovieDetails(title));
+    Promise.all(moviePromises).then(movies => {
+        console.log('All movies fetched:', movies);
+        displayMovies(movies);
+    });
 }
 function saveFavoriteMovies(favorites) {
-  localStorage.setItem('favoriteMovies', JSON.stringify(favorites));
+    localStorage.setItem('favoriteMovies', JSON.stringify(favorites));
 }
 
 function loadFavoriteMovies() {
-  const favoritesJSON = localStorage.getItem('favoriteMovies');
-  return JSON.parse(favoritesJSON) || [];
+    const favoritesJSON = localStorage.getItem('favoriteMovies');
+    return JSON.parse(favoritesJSON) || [];
 }
 
 fetchAndDisplayMovies();
